3316e0bf301a7f4407c90fa8cb1305947853b55e
Arna2Guardia <117608420+Arna2Guardia@users.noreply.github.com>
2023-01-31 01:44:26 +0100

    Update jeu_de_lav_vie.py

--- jeu_de_lav_vie.py
+++ jeu_de_lav_vie.py
@@ -1,25 +1,59 @@
-import random
+import random, os, time
 x = 5
 y = 5
 
-def init_tab(x,y):
-    tab = [[0] * x for i in range(x)]
-    for i in range(x):
-        for j in range(y):
-            tab[i][j] = str(random.randint(0,1))
+tab = [[0] * x for i in range(x)]
+tab2 = [[0] * x for i in range(x)]
+for i in range(x):
+    for j in range(y):
+        tab[i][j] = random.randint(0,1)
+        tab2[i][j] = tab[i][j]
 
-    #print(tab)
-    return tab
 
-def print_tab(x,y):
-    tab = init_tab(x,y)
+def printTab(x,y, tab):
     for i in range(x):
         print(tab[i])
 
 
-print_tab(x,y)
-#init_tab(5)
+def cell(x,y,tab):
+    return tab[y][x]
 
+printTab(x,y, tab)
+print('\n \n \n')
+
+while True:
+    os.system('cls')
+    for i in range(1, x-1):
+        for j in range(1, y-1):
+            cpt = 0
+            if tab[i-1][j-1] == 1:
+                cpt += 1
+            if tab[i][j-1] == 1:
+                cpt += 1
+            if tab[i+1][j-1] == 1:
+                cpt += 1
+            if tab[i-1][j] == 1:
+                cpt += 1
+            if tab[i+1][j] == 1:
+                cpt += 1
+            if tab[i-1][j+1] == 1:
+                cpt += 1
+            if tab[i][j+1] == 1:
+                cpt += 1
+            if tab[i+1][j+1] == 1:
+                cpt += 1
+
+            if cpt > 3 or cpt < 2:
+                tab2[i][j] = 0
+            elif cpt == 3:
+                tab2[i][j] = 1
+            else:
+                tab2[i][j] = tab[i][j]
+
+    tab = [row[:] for row in tab2]
+    printTab(x,y, tab)
+    print('\n')
+    time.sleep(0.5)
 
 
 
